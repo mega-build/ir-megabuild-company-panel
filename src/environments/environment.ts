eb4934bbb3752f630e711367f11bbe3451593e83
mohsenXAD <mohsenxad@gmail.com>
2023-10-02 19:02:42 +0330

    change url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = 
 {
     production: true,
-    //API_URL: 'https://panelapi.megabuild.ir',
-    API_URL: 'http://localhost:2001'
+    API_URL: 'https://panelapi.megabuild.ir',
+    //API_URL: 'http://localhost:2001'
 };
