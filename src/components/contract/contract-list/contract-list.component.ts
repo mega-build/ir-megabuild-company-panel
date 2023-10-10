85931c453dfccde5593ea4a21af80f88808d21c0
mohsenXAD <mohsenxad@gmail.com>
2023-10-10 12:20:59 +0330

    add sort contract list

--- src/components/contract/contract-list/contract-list.component.ts
+++ src/components/contract/contract-list/contract-list.component.ts
@@ -10,5 +10,87 @@ import { Component, Input } from '@angular/core';
 
 export class ContractListComponent
 	{
-	 	@Input() contractList: any[]=[];
+	 	@Input() contractList!: any[];
+
+		contractDateSortType:number = 1;
+		contractFinishDateSortType:number = 1;
+
+		sortByContractDate
+		():void
+			{
+				this.contractDateSortType = this.contractDateSortType * -1;
+				this.contractList.sort(this.compareContractDate);
+			}
+
+		sortByContractFinishDate
+		():void
+			{
+				this.contractFinishDateSortType = this.contractFinishDateSortType * -1;
+				this.contractList.sort(this.compareContractFinishDate);
+			}
+
+		getSortetContractList
+		():any[]
+			{
+				if
+				(
+					this.contractList
+				)
+					{
+
+						return this.contractList.sort(this.compareContractDate).sort(this.compareContractFinishDate);
+					}
+				else
+					{
+						return [];
+					}
+			}
+
+		compareContractDate = 
+		(
+			contractOne:any,
+			contractTwo:any
+		):number =>
+			{
+				if
+				(
+					contractOne.contractDate < contractTwo.contractDate 
+				)
+					{
+						return (-1 * this.contractDateSortType);
+					}
+				else if
+				(
+					contractOne.contractDate > contractTwo.contractDate
+				)
+					{
+						return this.contractDateSortType;
+					}
+
+				return 0;
+			}
+
+		compareContractFinishDate =
+		(
+			contractOne:any,
+			contractTwo:any
+		):number =>
+			{
+				if
+				(
+					contractOne.contractFinishDate < contractTwo.contractFinishDate 
+				)
+					{
+						return (-1 * this.contractFinishDateSortType);
+					}
+				else if
+				(
+					contractOne.contractFinishDate > contractTwo.contractFinishDate
+				)
+					{
+						return this.contractFinishDateSortType;
+					}
+
+				return 0;
+			}
 	}
